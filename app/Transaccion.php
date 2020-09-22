5b6383ad3e14062a45491214e733601b9c24146e
yeini <yportegar@ufpso.edu.co>
2020-09-22 11:52:06 -0500

    Initial commit

---
+++ app/Transaccion.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaccion extends Model
+{
+    //
+    protected $table = 'transacciones';
+
+    protected $fillable = ['user_id','fecha','tipo_transaccion','monto'];
+    
+}
